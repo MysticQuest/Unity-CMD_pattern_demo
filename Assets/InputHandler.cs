b310c1bbf0b5f988d72b694d92dcaa24e509e77f
Mystic Quest <l1skou@gmail.com>
2019-12-15 23:26:46 +0200

    created input handler

---
+++ Assets/InputHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    public GameObject actor;
+    Animator anim;
+    Command keyQ, keyE, keyW;
+
+
+    void Start()
+    {
+        keyQ = new Jump();
+        keyW = new DoNothing();
+        keyE = new DoNothing();
+        anim = actor.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            keyQ.Execute(anim);
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            keyW.Execute(anim);
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            keyE.Execute(anim);
+        }
+    }
+}
