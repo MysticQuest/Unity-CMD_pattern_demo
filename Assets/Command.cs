5f762873c420c86f77d1ee2cc5cbca76010d2fdb
Mystic Quest <l1skou@gmail.com>
2019-12-16 00:06:56 +0200

    replay functionality

--- Assets/Command.cs
+++ Assets/Command.cs
@@ -7,11 +7,35 @@ public abstract class Command
     public abstract void Execute(Animator anim);
 }
 
+public class MoveForward : Command
+{
+    public override void Execute(Animator anim)
+    {
+        anim.SetTrigger("isWalking");
+    }
+}
+
 public class Jump : Command
 {
     public override void Execute(Animator anim)
     {
         anim.SetTrigger("isJumping");
+    }
+}
+
+public class Kick : Command
+{
+    public override void Execute(Animator anim)
+    {
+        anim.SetTrigger("isKicking");
+    }
+}
+
+public class Punch : Command
+{
+    public override void Execute(Animator anim)
+    {
+        anim.SetTrigger("isPunching");
     }
 }
 
--- Assets/InputHandler.cs
+++ Assets/InputHandler.cs
@@ -6,30 +6,88 @@ public class InputHandler : MonoBehaviour
 {
     public GameObject actor;
     Animator anim;
-    Command keyQ, keyE, keyW;
+    Command keyQ, keyE, keyW, upArrow;
+    List<Command> oldCommands = new List<Command>();
+
+    Coroutine replayCoroutine;
+    bool shouldStartReplay;
+    bool isReplaying;
+
+
+
 
 
     void Start()
     {
         keyQ = new Jump();
-        keyW = new DoNothing();
-        keyE = new DoNothing();
+        keyW = new Punch();
+        keyE = new Kick();
+        upArrow = new MoveForward();
         anim = actor.GetComponent<Animator>();
+        Camera.main.GetComponent<CameraFollow360>().player = actor.transform;
+
     }
 
     void Update()
     {
+        if (!isReplaying)
+            HandleInput();
+        StartReplay();
+
+
+    }
+
+    void HandleInput()
+    {
         if (Input.GetKeyDown(KeyCode.Q))
         {
             keyQ.Execute(anim);
+            oldCommands.Add(keyQ);
         }
         else if (Input.GetKeyDown(KeyCode.W))
         {
             keyW.Execute(anim);
+            oldCommands.Add(keyW);
         }
         else if (Input.GetKeyDown(KeyCode.E))
         {
             keyE.Execute(anim);
+            oldCommands.Add(keyE);
         }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            upArrow.Execute(anim);
+            oldCommands.Add(upArrow);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            shouldStartReplay = true;
+        }
+    }
+
+    void StartReplay()
+    {
+        if (shouldStartReplay && oldCommands.Count > 0)
+        {
+            shouldStartReplay = false;
+            if (replayCoroutine != null)
+            {
+                StopCoroutine(replayCoroutine);
+            }
+            replayCoroutine = StartCoroutine(ReplayCommands());
+
+        }
+    }
+
+    IEnumerator ReplayCommands()
+    {
+        isReplaying = true;
+        for (int i = 0; i < oldCommands.Count; i++)
+        {
+            oldCommands[i].Execute(anim);
+            yield return new WaitForSeconds(1f);
+        }
+        isReplaying = false;
     }
 }
